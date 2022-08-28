24d2a4bf24f2a4a48679a3dca0d1a685ab0a6e54
ykim <ykim5867@gmail.com>
2022-08-28 13:29:09 +0900

    220828

---
+++ Bronze/BV_2475/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] number = new int[5];
+        for(int i = 0; i < 5; i++) {
+            number[i] = sc.nextInt();
+        }
+        int numVerify = 0;
+        for(int i = 0; i < 5; i++) {
+            numVerify += number[i] * number[i];
+        }
+        numVerify %= 10;
+        System.out.println(numVerify);
+        sc.close();
+    }
+}
