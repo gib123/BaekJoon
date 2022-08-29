d36e6fe8654e3e97980b6a7f1ce2fcc51d5276ad
ykim <ykim5867@gmail.com>
2022-08-29 10:43:14 +0900

    220829

---
+++ Bronze/BV_10699/Main.java
@@ -0,0 +1,11 @@
+import java.util.Calendar;
+import java.text.SimpleDateFormat;
+
+public class Main {
+    public static void main(String[] args) {
+        Calendar today = Calendar.getInstance();
+        today.add(Calendar.HOUR, 3);
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(dateFormat.format(today.getTime()));
+    }
+}
