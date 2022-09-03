e1d59426345b469c69097270fd6afeb2bc4ac4ab
ykim <ykim5867@gmail.com>
2022-09-03 13:22:37 +0900

    220903

---
+++ Bronze/BV_4999/Main.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int heLen = br.readLine().length();
+        int doctorLen = br.readLine().length();
+
+        String result = (heLen >= doctorLen) ? "go" : "no";
+        System.out.println(result);
+        br.close();
+    }
+}
