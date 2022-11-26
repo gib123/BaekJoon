2b0654ef84e385a7035f2a66436277300851897a
ykim-yeji <ykim5867@gmail.com>
2022-11-26 17:13:48 +0900

    feat: solve BaekJoon Bronze V no24568

---
+++ BaekJoon/bronze/v/no24568/Main.java
@@ -0,0 +1,13 @@
+package bronze.v.no24568;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int R = Integer.parseInt(br.readLine());
+        int S = Integer.parseInt(br.readLine());
+        System.out.println(R * 8 + S * 3 - 28);
+        br.close();
+    }
+}
