e96076a3b12ddd281cb0e7653ea6f5d5c1946463
ykim-yeji <ykim5867@gmail.com>
2022-11-28 22:43:02 +0900

    feat: solve BaekJoon Bronze V no1000

---
+++ BaekJoon/bronze/v/no1000/Main.java
@@ -0,0 +1,17 @@
+package bronze.v.no1000;
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int A, B;
+
+        Scanner sc = new Scanner(System.in);
+        A = sc.nextInt();
+        B = sc.nextInt(); //(0 < A, B < 10)
+
+        System.out.println(A + B);
+    }
+
+}
