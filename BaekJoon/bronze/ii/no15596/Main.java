16439a72e86c40f9e7ad63622df22eaa55e71ea7
ykim-yeji <ykim5867@gmail.com>
2022-12-01 14:07:41 +0900

    feat: solve BaekJoon Bronze II no15596

---
+++ BaekJoon/bronze/ii/no15596/Main.java
@@ -0,0 +1,11 @@
+package bronze.ii.no15596;
+
+public class Main {
+    long sum(int[] a) {
+        long ans = 0;
+        for (int i = 0; i < a.length; i++) {
+            ans += a[i];
+        }
+        return ans;
+    }
+}
