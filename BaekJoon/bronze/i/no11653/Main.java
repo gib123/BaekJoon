9616a43be967f197fea3c879a1b8f0600c13bb33
ykim-yeji <ykim5867@gmail.com>
2022-12-07 13:42:42 +0900

    feat: solve BaekJoon Bronze I no11653

---
+++ BaekJoon/bronze/i/no11653/Main.java
@@ -0,0 +1,22 @@
+package bronze.i.no11653;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 2; i <= Math.sqrt(N); i++) {
+            while (N % i == 0) {
+                sb.append(i).append("\n");
+                N /= i;
+            }
+        }
+        if (N != 1) {
+            sb.append(N);
+        }
+        System.out.println(sb);
+    }
+}
