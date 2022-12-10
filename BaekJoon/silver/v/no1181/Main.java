d7e46bad8e3ae2fbbdc06a07dae7778f68023c54
ykim-yeji <ykim5867@gmail.com>
2022-12-10 17:46:27 +0900

    feat: solve BaekJoon Silver V no1181

---
+++ BaekJoon/silver/v/no1181/Main.java
@@ -0,0 +1,32 @@
+package silver.v.no1181;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        String[] arr = new String[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = br.readLine();
+        }
+        Arrays.sort(arr, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() == s2.length()) return s1.compareTo(s2);
+                else return s1.length() - s2.length();
+            }
+        });
+        StringBuilder sb= new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            if (i == 0 || !arr[i - 1].equals(arr[i])) {
+                sb.append(arr[i] + "\n");
+            }
+        }
+        System.out.println(sb);
+    }
+}
