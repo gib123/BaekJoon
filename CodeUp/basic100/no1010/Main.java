1ddb415fbd78d5d1f358fc00150bba915c2874f0
ykim-yeji <ykim5867@gmail.com>
2022-12-15 16:42:16 +0900

    feat: solve CodeUp Basic100 no1010

---
+++ CodeUp/basic100/no1010/Main.java
@@ -0,0 +1,13 @@
+package basic100.no1010;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args) {
+        int num;
+        Scanner s = new Scanner(System.in);
+
+        num = s.nextInt();
+        System.out.println(num);
+    }
+}
