b8372efd9a3a4417e3598a15f2d9563ebda5549c
ykim-yeji <ykim5867@gmail.com>
2022-12-15 16:42:27 +0900

    feat: solve CodeUp Basic100 no1012

---
+++ CodeUp/basic100/no1012/Main.java
@@ -0,0 +1,13 @@
+package basic100.no1012;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args) {
+        float n;
+        Scanner s = new Scanner(System.in);
+
+        n = s.nextFloat();
+        System.out.printf("%.6f\n", n);
+    }
+}
