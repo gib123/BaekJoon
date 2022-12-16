5ddc3a22ea3ea642274c4f7e07599a07aa220e28
ykim-yeji <ykim5867@gmail.com>
2022-12-16 19:59:31 +0900

    feat: solve CodeUp Basic100 no1017

---
+++ CodeUp/basic100/no1017/Main.java
@@ -0,0 +1,13 @@
+package basic100.no1017;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args) {
+        int n;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        System.out.println(n + " " + n + " " + n);
+    }
+}
