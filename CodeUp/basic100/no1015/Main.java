a25b0d50117ba5e6e7b52abaa83120cb8e063cc8
ykim-yeji <ykim5867@gmail.com>
2022-12-16 19:59:26 +0900

    feat: solve CodeUp Basic100 no1015

---
+++ CodeUp/basic100/no1015/Main.java
@@ -0,0 +1,14 @@
+package basic100.no1015;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[]args) {
+        float n;
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextFloat();
+
+        System.out.printf("%.2f\n", n);
+    }
+}
