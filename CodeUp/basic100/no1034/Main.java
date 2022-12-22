5f96fd9ede167dbe0b7beba44598c203e86525f7
ykim-yeji <ykim5867@gmail.com>
2022-12-22 12:02:03 +0900

    feat: solve CodeUp Basic100 no1034

---
+++ CodeUp/basic100/no1034/Main.java
@@ -0,0 +1,16 @@
+package basic100.no1034;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String o;
+        int n;
+
+        o = sc.nextLine();
+        n = Integer.valueOf(o, 8);
+
+        System.out.println(n);
+    }
+}
