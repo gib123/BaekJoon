07f0a0811efcf583fa0b2ac47cd015802eeeecbc
ykim-yeji <ykim5867@gmail.com>
2022-12-25 17:16:04 +0900

    feat: solve CodeUp Basic100 no1044

---
+++ CodeUp/basic100/no1044/Main.java
@@ -0,0 +1,13 @@
+package basic100.no1044;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a;
+
+        a = sc.nextInt();
+        System.out.printf("%d\n", (long)a + 1);
+    }
+}
