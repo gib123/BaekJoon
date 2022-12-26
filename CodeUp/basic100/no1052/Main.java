41bf7a9021d5115c230548fefe3eef319697dd1e
ykim-yeji <ykim5867@gmail.com>
2022-12-26 13:46:44 +0900

    feat: solve CodeUp Basic100 no1052

---
+++ CodeUp/basic100/no1052/Main.java
@@ -0,0 +1,21 @@
+package basic100.no1052;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a, b, isPrime = 0;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if(a != b) {
+            isPrime = 1;
+        }
+
+        System.out.println(isPrime);
+
+        sc.close();
+    }
+}
