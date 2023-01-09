a915b422e4026cc84592f073212a9b69794859fa
ykim-yeji <ykim5867@gmail.com>
2023-01-09 14:07:35 +0900

    feat: solve CodeUp Basic100 no1072

---
+++ CodeUp/basic100/no1072/Main.java
@@ -0,0 +1,16 @@
+package basic100.no1072;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        while(n-- != 0) {
+            int m = sc.nextInt();
+            System.out.println(m);
+        }
+    }
+}
