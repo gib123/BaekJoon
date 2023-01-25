862b9cfdbfe99fdfeb39273bb42c3315d7399376
ykim-yeji <ykim5867@gmail.com>
2023-01-25 13:31:35 +0900

    feat: solve Programmers Lv2 최댓값과 최솟값

---
+++ Programmers/Lv2/최댓값과_최솟값/Solution.java
@@ -0,0 +1,16 @@
+package Lv2.최댓값과_최솟값;
+
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String s) {
+        String[] str = s.split(" ");
+        int[] num = new int[str.length];
+        for (int i = 0; i < str.length; i++) {
+            num[i] = Integer.parseInt(str[i]);
+        }
+        Arrays.sort(num);
+        String answer = num[0] + " " + num[num.length - 1];
+        return answer;
+    }
+}
