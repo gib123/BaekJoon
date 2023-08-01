15a913b81960308efd0d987af10f56b8d614e01a
ykim-yeji <ykim5867@gmail.com>
2023-08-01 19:53:47 +0900

    refactor: solve SWEA D3 no1208

--- SW_Expert_Academy/D3/no1208/Solution.java
+++ SW_Expert_Academy/D3/no1208/Solution.java
@@ -1,38 +1,27 @@
 package D3.no1208;
 
+import java.util.*;
 import java.io.*;
 
 class Solution {
     public static void main(String args[]) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        for(int test_case = 1; test_case <= 10; test_case++) {
+        for (int test_case = 1; test_case <= 10; test_case++) {
             int count = Integer.parseInt(br.readLine());
-            String[] stHeight = br.readLine().split(" ");
-            int[] height = new int[100];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int[] box = new int[100];
             for (int i = 0; i < 100; i++) {
-                height[i] = Integer.parseInt(stHeight[i]);
+                box[i] = Integer.parseInt(st.nextToken());
             }
-            int gap = Integer.MAX_VALUE;
-            while (true) {
-                int maxIndex = 0;
-                int minIndex = 0;
-                for (int i = 0; i < 100; i++) {
-                    if (height[i] > height[maxIndex]) {
-                        maxIndex = i;
-                    }
-                    if (height[i] < height[minIndex]) {
-                        minIndex = i;
-                    }
-                }
-                gap = height[maxIndex] - height[minIndex];
-                if (count <= 0 || gap <= 1) {
-                    break;
-                }
-                height[maxIndex]--;
-                height[minIndex]++;
-                count--;
+            Arrays.sort(box);
+            int diff = box[box.length - 1] - box[0];
+            while (count-- > 0 && diff > 1) {
+                box[box.length - 1]--;
+                box[0]++;
+                Arrays.sort(box);
+                diff = box[box.length - 1] - box[0];
             }
-            System.out.println("#" + test_case + " " + gap);
+            System.out.println("#" + test_case + " " + diff);
         }
     }
 }
