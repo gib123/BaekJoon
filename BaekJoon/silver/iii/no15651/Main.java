0897623cb65f5357c31727336fc59aa3e0e10da9
ykim-yeji <ykim5867@gmail.com>
2023-08-11 23:51:12 +0900

    feat: solve BaekJoon Silver III no15651

---
+++ BaekJoon/silver/iii/no15651/Main.java
@@ -0,0 +1,32 @@
+package silver.iii.no15651;
+
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int N;
+    static int M;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        char[] arr = new char[2 * M];
+        Arrays.fill(arr, ' ');
+        recur(0, arr);
+        System.out.print(sb);
+    }
+
+    public static void recur(int n, char[] arr) {
+        if (n == M) {
+            sb.append(arr).append("\n");
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            arr[n * 2] = (char)(i + 1 + '0');
+            recur(n + 1, arr);
+        }
+    }
+}
