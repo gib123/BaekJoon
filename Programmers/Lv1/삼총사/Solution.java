2a2cf9b95963afa2a319ab490f4d54638abe3ec7
ykim-yeji <ykim5867@gmail.com>
2023-10-09 22:55:29 +0900

    feat: solve Programmers Lv1 삼총사

---
+++ Programmers/Lv1/삼총사/Solution.java
@@ -0,0 +1,15 @@
+package Lv1.삼총사;
+
+public class Solution {
+    public int solution(int[] number) {
+        int count = 0;
+        for (int i = 0; i < number.length; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                for (int k = j + 1; k < number.length; k++) {
+                    if (number[i] + number[j] + number[k] == 0) count++;
+                }
+            }
+        }
+        return count;
+    }
+}
