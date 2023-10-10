d4ed24a5da25d721654a0e7424243a4af5715ded
ykim-yeji <ykim5867@gmail.com>
2023-10-10 20:29:45 +0900

    feat: solve Programmers Lv1 문자열 내 마음대로 정렬하기

---
+++ Programmers/Lv1/문자열_내_마음대로_정렬하기/Solution.java
@@ -0,0 +1,18 @@
+package Lv1.문자열_내_마음대로_정렬하기;
+
+import java.util.*;
+
+public class Solution {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                if (o1.charAt(n) == o2.charAt(n)) {
+                    return o1.compareTo(o2);
+                }
+                return o1.charAt(n) - o2.charAt(n);
+            }
+        });
+        return strings;
+    }
+}
