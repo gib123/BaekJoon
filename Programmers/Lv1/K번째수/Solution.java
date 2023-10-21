7c436345c7f599472194c533c57132ae92b1d006
ykim-yeji <ykim5867@gmail.com>
2023-10-21 16:35:44 +0900

    feat: solve Programmers Lv1 K번째수

---
+++ Programmers/Lv1/K번째수/Solution.java
@@ -0,0 +1,15 @@
+package Lv1.K번째수;
+
+import java.util.*;
+
+public class Solution {
+	public int[] solution(int[] array, int[][] commands) {
+		int[] answer = new int[commands.length];
+		for (int i = 0; i < commands.length; i++) {
+			int[] copy = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
+			Arrays.sort(copy);
+			answer[i] = copy[commands[i][2] - 1];
+		}
+		return answer;
+	}
+}
