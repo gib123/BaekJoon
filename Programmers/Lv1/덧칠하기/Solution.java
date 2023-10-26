c6a3662a4ce7360f4570d31a0b069ca46b04bd27
ykim-yeji <ykim5867@gmail.com>
2023-10-26 23:49:49 +0900

    feat: solve Programmers Lv1 덧칠하기

--- Programmers/Lv1/덧칠하기/Solution.java
+++ Programmers/Lv1/덧칠하기/Solution.java
@@ -1,4 +1,15 @@
 package Lv1.덧칠하기;
 
 public class Solution {
+	public int solution(int n, int m, int[] section) {
+		int count = 1;
+		int start = section[0];
+		for (int i = 1; i < section.length; i++) {
+			if (section[i] >= start + m) {
+				count++;
+				start = section[i];
+			}
+		}
+		return count;
+	}
 }
