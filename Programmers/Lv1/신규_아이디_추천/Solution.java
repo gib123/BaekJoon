7b15632475863e3de737c7f713ca0bf78aa43ae1
ykim-yeji <ykim5867@gmail.com>
2023-11-03 20:35:04 +0900

    feat: solve Programmers Lv1 신규 아이디 추천

---
+++ Programmers/Lv1/신규_아이디_추천/Solution.java
@@ -0,0 +1,21 @@
+package Lv1.신규_아이디_추천;
+
+public class Solution {
+	public String solution(String new_id) {
+		new_id = new_id.toLowerCase()
+			.replaceAll("[^a-z0-9-_.]", "")
+			.replaceAll("[.]{2,}", ".")
+			.replaceAll("^[.]|[.]$", "");
+		if (new_id.isEmpty()) new_id += "a";
+		if (new_id.length() >= 16) {
+			new_id = new_id.substring(0, 15).replaceAll("[.]$", "");
+		}
+		if (new_id.length() <= 2) {
+			char ch = new_id.charAt(new_id.length() - 1);
+			while (new_id.length() <= 2) {
+				new_id += ch;
+			}
+		}
+		return new_id;
+	}
+}
